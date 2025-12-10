5b24e298b9c93cea2813ccbc0439767871c4200a
RichieBoyle1 <30044728@live.nwrc.ac.uk>
2025-12-10 11:40:18 +0000

    Update app.js
    Updated App.js to rectify changes

--- app.js
+++ app.js
@@ -62,45 +62,7 @@ if (contactForm) {
         alert("Message sent successfully!");
         contactForm.reset(); // Clear the form
     });
-
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-
-    const form = document.getElementById("contact-form");
-
-
-    form.addEventListener("submit", function (e) {
-
-        downloadFormData(form);
-        setTimeout(() => {
-            window.location.href = "thankyou.html";
-        }, 300);
-    });
-
-    function downloadFormData(form) {
-        const formData = new FormData(form);
-        let text = "";
-
-        for (let [key, value] of formData.entries()) {
-            text += `${key}: ${value}\n`;
-        }
-
-        console.log("Saving data:", text);
-
-        const blob = new Blob([text], { type: "text/plain" });
-        const url = URL.createObjectURL(blob);
-
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "form-data.txt";
-        document.body.appendChild(a);
-
-        a.click();   // this triggers download
-        a.remove(); //we create a temp URL download so remove it after
-
-        URL.revokeObjectURL(url);
-    }
-
-});
  
+
